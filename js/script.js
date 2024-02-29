3554d6448a7269e7465ddee9d0c770e7a1619741
Mohammed Saad Abrar <161331536+saadabrar7@users.noreply.github.com>
2024-02-29 21:52:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ const playMusic = (track, pause = false) => {
 
 
 async function displayAlbums() {
-  let a = await fetch(`spotifyclone/songs/`);
+  let a = await fetch(`../songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -95,13 +95,13 @@ async function displayAlbums() {
   let cardcontainer = document.querySelector(".cardcontainer");
 
   Array.from(anchors).forEach(async e => {
-    if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
+    if (e.href.includes("../songs") && !e.href.includes(".htaccess")) {
       let folder = e.href.split("/").slice(-1)[0];
-      let a = await fetch(`spotifyclone/songs/${folder}/info.json`);
+      let a = await fetch(`../songs/${folder}/info.json`);
       let jsonResponse = await a.json();
 
       cardcontainer.innerHTML += `<div data-folder="${folder}" class="card">
-        <img src="spotifyclone/songs/${folder}/cover.jpg" alt="">
+        <img src="../songs/${folder}/cover.jpg" alt="">
         <h2>${jsonResponse.title}</h2>
         <p>${jsonResponse.discription}</p>
       </div>`;
@@ -112,7 +112,7 @@ async function displayAlbums() {
   cardcontainer.addEventListener("click", async (event) => {
     const clickedCard = event.target.closest(".card");
     if (clickedCard) {
-      songs = await getsongs(`spotifyclone/songs/${clickedCard.dataset.folder}`);
+      songs = await getsongs(`../songs/${clickedCard.dataset.folder}`);
         document.querySelector(".left").style.left = "0"
       }
     // playMusic(songs[0])
@@ -122,8 +122,7 @@ async function displayAlbums() {
 
 
 async function main() {
-  await getsongs("spotifyclone/songs/ncs");
-  playMusic(songs[0], true)
+  await getsongs("../songs/ncs");
 
  displayAlbums()
 
